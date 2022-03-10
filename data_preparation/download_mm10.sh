858e6265e7cc1dd932071be4ee692e831ee33b62
Doruk Cakmakci <doruk.cakmakci@outlook.com>
2022-03-09 23:39:28 -0500

    add .gitignore, genome download and preparation scripts for hg19, hg38 and mm10

---
+++ data_preparation/download_mm10.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+
+# prepare data folder for downloads
+mkdir -p ../data
+cd ../data
+mkdir -p mm10
+cd mm10
+
+# download sequence information of chromosomes one by one 
+# from https://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr1.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr2.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr3.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr4.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr5.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr6.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr7.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr8.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr9.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr10.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr11.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr12.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr13.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr14.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr15.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr16.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr17.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr18.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr19.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr20.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr21.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chr22.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chrX.fa.gz
+wget http://hgdownload.soe.ucsc.edu/goldenPath/mm10/chromosomes/chrY.fa.gz
+
+# extract the downloaded files
+gunzip chr1.fa.gz
+gunzip chr2.fa.gz
+gunzip chr3.fa.gz
+gunzip chr4.fa.gz
+gunzip chr5.fa.gz
+gunzip chr6.fa.gz
+gunzip chr7.fa.gz
+gunzip chr8.fa.gz
+gunzip chr9.fa.gz
+gunzip chr10.fa.gz
+gunzip chr11.fa.gz
+gunzip chr12.fa.gz
+gunzip chr13.fa.gz
+gunzip chr14.fa.gz
+gunzip chr15.fa.gz
+gunzip chr16.fa.gz
+gunzip chr17.fa.gz
+gunzip chr18.fa.gz
+gunzip chr19.fa.gz
+gunzip chr20.fa.gz
+gunzip chr21.fa.gz
+gunzip chr22.fa.gz
+gunzip chrX.fa.gz
+gunzip chrY.fa.gz
+
